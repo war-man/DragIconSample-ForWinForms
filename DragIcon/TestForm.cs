07e9207be0da52b1cd4f0bf45aebfd377af9b89e
Mike-ThinkGeo <mikeyu@thinkgeo.com>
2016-12-27 19:32:58 +0800

    Upload code.

---
+++ DragIcon/TestForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Drawing;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Styles;
+using ThinkGeo.MapSuite.WinForms;
+
+namespace DraggingIcon
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            winformsMap1.MapUnit = GeographyUnit.DecimalDegree;
+            winformsMap1.CurrentExtent = new RectangleShape(-97.7591, 30.3126, -97.7317, 30.2964);
+            winformsMap1.BackgroundOverlay.BackgroundBrush = new GeoSolidBrush(GeoColor.FromArgb(255, 198, 255, 255));
+
+            //Displays the World Map Kit as a background.
+            ThinkGeo.MapSuite.WinForms.WorldMapKitWmsDesktopOverlay worldMapKitDesktopOverlay = new ThinkGeo.MapSuite.WinForms.WorldMapKitWmsDesktopOverlay();
+            winformsMap1.Overlays.Add(worldMapKitDesktopOverlay);
+
+            //EditInteractiveOverlay used because it already have the logic for dragging.
+            EditInteractiveOverlay editInteractiveOverlay = new EditInteractiveOverlay();
+
+            //Sets the property IsActive for DragControlPointsLayer to false so that the control point (as four arrows) is not visible.
+            editInteractiveOverlay.DragControlPointsLayer.ZoomLevelSet.ZoomLevel01.DefaultPointStyle.IsActive = false;
+            editInteractiveOverlay.DragControlPointsLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            //Sets the property IsActive for all the Styles of EditShapesLayer because we are using a ValueStyle instead.
+            editInteractiveOverlay.EditShapesLayer.ZoomLevelSet.ZoomLevel01.DefaultPointStyle.IsActive = false; 
+            editInteractiveOverlay.EditShapesLayer.ZoomLevelSet.ZoomLevel01.DefaultLineStyle.IsActive = false; 
+            editInteractiveOverlay.EditShapesLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle.IsActive = false; 
+
+            //ValueStyle used for displaying the feature according to the value of the column "Type" for displaying with a bus or car icon.
+            ValueStyle valueStyle = new ValueStyle();
+            valueStyle.ColumnName = "Type";
+
+            PointStyle carPointStyle = new PointStyle(new GeoImage("../../data/car_normal.png"));
+            carPointStyle.PointType = PointType.Bitmap;
+            PointStyle busPointStyle = new PointStyle(new GeoImage("../../data/bus_normal.png"));
+            busPointStyle.PointType = PointType.Bitmap;
+
+            valueStyle.ValueItems.Add(new ValueItem("Car", carPointStyle));
+            valueStyle.ValueItems.Add(new ValueItem("Bus", busPointStyle));
+
+            editInteractiveOverlay.EditShapesLayer.ZoomLevelSet.ZoomLevel01.CustomStyles.Add(valueStyle);
+            editInteractiveOverlay.EditShapesLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            editInteractiveOverlay.EditShapesLayer.Open();
+            editInteractiveOverlay.EditShapesLayer.Columns.Add(new FeatureSourceColumn("Type"));
+            editInteractiveOverlay.EditShapesLayer.Close();
+
+            Feature carFeature = new Feature(new PointShape(-97.7507, 30.3092));
+            carFeature.ColumnValues["Type"] = "Car";
+
+            Feature busFeature = new Feature(new PointShape(-97.7428, 30.3053));
+            busFeature.ColumnValues["Type"] = "Bus";
+
+            editInteractiveOverlay.EditShapesLayer.InternalFeatures.Add("Car", carFeature);
+            editInteractiveOverlay.EditShapesLayer.InternalFeatures.Add("Bus", busFeature);
+
+            //Sets the properties of EditInteractiveOverlay to have the appropriate behavior.
+            //Make sure CanDrag is set to true.
+            editInteractiveOverlay.CanAddVertex = false;
+            editInteractiveOverlay.CanDrag = true;
+            editInteractiveOverlay.CanRemoveVertex = false;
+            editInteractiveOverlay.CanResize = false;
+            editInteractiveOverlay.CanRotate = false;
+            editInteractiveOverlay.CalculateAllControlPoints();
+
+            winformsMap1.EditOverlay = editInteractiveOverlay;
+
+            winformsMap1.Refresh();
+        }
+      
+        private void winformsMap1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Displays the X and Y in screen coordinates.
+            statusStrip1.Items["toolStripStatusLabelScreen"].Text = "X:" + e.X + " Y:" + e.Y;
+
+            //Gets the PointShape in world coordinates from screen coordinates.
+            PointShape pointShape = ExtentHelper.ToWorldCoordinate(winformsMap1.CurrentExtent, new ScreenPointF(e.X, e.Y), winformsMap1.Width, winformsMap1.Height);
+
+            //Displays world coordinates.
+            statusStrip1.Items["toolStripStatusLabelWorld"].Text = "(world) X:" + Math.Round(pointShape.X, 4) + " Y:" + Math.Round(pointShape.Y, 4);
+        }
+        
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Dragging Icon Sample for WinForms
+
+### Description
+
+In today’s sample, we show how to drag icons representing vehicle on the map. This is a handy feature if you want to give your users the ability to drag and drop some non stationary features such as vehicles. You can see that to accomplish this functionality, you can use EditInteractiveOverlay as it already has all the necessary logic for dragging purposes. Look at the code to see how to set up that overlay to have the expected behavior.
+
+![Screenshot](https://github.com/ThinkGeo/DraggingIconSample-ForWinForms/blob/master/ScreenShot.png)
+
+### Requirements
+This sample makes use of the following NuGet Packages
+
+[MapSuite 10.0.0.0](http:mapsuite.nuget)
+
+### About the Code
+
+Working...
+
+### Getting Help
+
+[Map Suite Desktop for Winforms Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_desktop_edition)
+
+[Map Suite Desktop for Winforms Product Description](http://thinkgeo.com/map-suite-developer-gis/desktop-edition/)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### Key APIs
+This example makes use of the following APIs:
+
+Working...
+
+### About Map Suite
+Map Suite is a set of powerful development components and services for the .Net Framework.
+
+### About ThinkGeo
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
