3eb2a5f2f37336f7a2eca451d20a61fe1b636530
RexLiu <rexliu@thinkgeo.com>
2017-01-17 15:39:13 +0800

    Modify assembly info.

--- DragIcon/Properties/AssemblyInfo.cs
+++ DragIcon/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DraggingIcon")]
+[assembly: AssemblyTitle("DragIcon")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DraggingIcon")]
+[assembly: AssemblyProduct("DragIcon")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
